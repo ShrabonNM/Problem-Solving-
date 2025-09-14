3e7ae36ea3bc893ccbce6fd0cc16b67a6f5a11f1
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-09-14 20:39:27 +0600

    Add files via upload

---
+++ print_all_negative_element_of_array.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+    int n ;
+    scanf("%d",&n);
+    int arr[n];
+
+
+    for(int i = 0 ; i<n ;i++){
+        scanf("%d",&arr[i]);
+    }
+    int d = 0;
+    int val = 0;
+    int n_arr[d];
+    for(int i = 0 ; i<n; i++ ){
+        if(arr[i]<val){
+
+            n_arr[d] = arr[i];
+            d++;
+        }
+    }
+
+
+      for(int i = 0 ; i<d ;i++){
+        printf("%d ",n_arr[i]);
+    }
+
+
+
+
+
+
+
+
+    return 0;
+}
