d1d6fbf63a06f2047a8e0f2f6d184ce0e76dfa18
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-09-17 20:05:37 +0600

    Add files via upload

---
+++ Identity_matrix.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int n ;
+    scanf("%d",&n);
+    int arr[n][n];
+    for(int i = 0 ; i<n ;i++){
+        for(int j = 0 ; j<n ;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int flag = 1;
+    for(int i = 0 ; i < n ; i++ ){
+        for(int j = 0 ; j<n ; j++){
+            if((i==j && arr[i][j]!=1) || (i!=j && arr[i][j]!=0)){
+                flag = 0;
+                break ;
+            }
+        }
+        if(flag==0 ) break ;
+    }
+
+
+    if(flag){
+        printf("Indentity Matrix \n ");
+    }
+    else {
+        printf("Not Identity Matrix\n");
+    }
+
+
+
+
+
+
+
+    return 0 ;
+}
