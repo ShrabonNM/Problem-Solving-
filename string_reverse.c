5c6838402acf55440a02759790b66880ee5218e0
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-10-01 16:12:37 +0600

    Create string_reverse.c

---
+++ string_reverse.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include<string.h>
+
+
+int main() {
+  
+    char str1[1000];
+    puts("Write something : ");
+    scanf("%s",str1);
+    int  size = 0;
+    int i = 0 ;
+    while(str1[i]!='\0'){
+        size++;
+        i++;
+    }
+    
+    for(int j = 0 , k = size - 1 ; j<=k ; j++,k-- ){
+        int temp = str1[j] ;
+        str1[j] = str1[k];
+        str1[k] = temp ;
+    }
+    puts("Revrse string is : ");
+    puts(str1);
+
+    return 0;
+}
