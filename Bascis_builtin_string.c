a8211d68e513f07276c4a3c32999d92ab1142406
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-10-03 00:17:49 +0600

    Update Bascis_builtin_string.c

--- Bascis_builtin_string.c
+++ Bascis_builtin_string.c
@@ -8,6 +8,9 @@ int main() {
     char s2[] = "How are you?";
     char s3[1000] = "I am studing at UIU ";
     char s4[1000] = "Where you are studing?";
+    char first_string[] ="Apple";
+    char second_string[] = "Banana";
+    printf("%d\n",strcmp(first_string , second_string));
     strcat(s3 , s4);
     printf("%s\n",s3); /*// array size should be more at least it can contain the other array which intended to assign/ concatinate here.*/
     strcpy(s2,s1);
