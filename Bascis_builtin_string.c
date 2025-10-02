4e93f08882739f4fcf6adf88cda2cc275477505e
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-10-02 22:05:21 +0600

    Create Bascis_builtin_string.c

---
+++ Bascis_builtin_string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<string.h>
+
+int main() {
+
+    char str[] = "Alhamdulillah";
+    char s1[] = "Hey Shrabon";
+    char s2[] = "How are you?";
+    char s3[1000] = "I am studing at UIU ";
+    char s4[1000] = "Where you are studing?";
+    strcat(s3 , s4);
+    printf("%s\n",s3); /*// array size should be more at least it can contain the other array which intended to assign/ concatinate here.*/
+    strcpy(s2,s1);
+    printf("%s\n",s2);
+    int x = strlen(str);
+    printf("%d\n",x);
+    return 0;
+}
