f6df66718122c978d72e9d5b51b5676ab98fd744
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-10-05 22:07:34 +0600

    Create string_sl_06.c

---
+++ string_sl_06.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+  char str[10000];
+  fgets(str , sizeof(str), stdin);
+  for(int i = 0 ; str[i]!='\0' ; i++){
+        if(str[i]=='\n') break ;
+       if(str[i]>='a' && str[i]<='z'){
+         str[i] = str[i] - 32;
+       }
+  }
+  puts(str);
+
+   return 0;
+}
