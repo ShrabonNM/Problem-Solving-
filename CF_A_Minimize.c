11e24b5c622ece0eb425a8fc48dba15568fb13d6
Noor Mohammed Shrabon <noormohammedshrabon@gmail.com>
2025-11-19 19:20:53 +0600

    Create CF_A_Minimize.c

---
+++ CF_A_Minimize.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--){
+       int a , b ;
+       scanf("%d %d",&a , &b);
+       int c = b - a ;
+       printf("%d\n",c);
+
+
+
+
+    }
+
+
+
+
+
+    return 0;
+}
